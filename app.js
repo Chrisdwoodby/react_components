88ba8e20b84214d8f9192a63e7640f3cd758a622
chrisdwoodby <chrisdwoodby@gmail.com>
2021-07-03 17:34:46 -0500

    dynamically rendered elements to page

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// TODO
+class GroceryList extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+
+  }
+
+  onClicked() {
+
+    this.setState({
+      done: !this.state.done
+    });
+
+  }
+
+  render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through wavy blue' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onClicked.bind(this)}>{this.props.food}</li>
+    );
+
+  }
+}
+
+var List = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <FoodList items = {['bread', 'carrots', 'salmon']}/>
+  </div>
+);
+
+var FoodList = (props) => (
+
+  <ul>
+    {props.items.map(food =>
+      <GroceryList food={food} />
+    )}
+  </ul>
+
+);
+
+
+ReactDOM.render(<List />, document.getElementById("app"));
